46bfe9ce6fb9c9bcb3c34432d4613b425d8ff8c4
Dror Poliak <drorp24@gmail.com>
2014-05-25 03:18:41 +0300

    validations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require rails.validations
-//= require rails.validations.simple_form
 //= require autoNumeric
 //= require jquery.validate.min
